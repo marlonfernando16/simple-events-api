8d6b26fa923df6121fd1e1a09ca2869d28cf5657
marlonfernando16 <marlon.fernando16@outlook.com>
2019-11-23 21:20:29 -0300

    comenta autenticacao para testes no frontend

--- api/views.py
+++ api/views.py
@@ -18,32 +18,32 @@ class EventoViewSet(viewsets.ModelViewSet):
 
     queryset = models.Evento.objects.all()
     serializer_class = serializers.EventoSerializer
-    permission_classes = [AppPermision]
+    # permission_classes = [AppPermision]
 
 
 class EspecialidadeViewSet(viewsets.ModelViewSet):
 
     queryset = models.Especialidade.objects.all()
     serializer_class = serializers.EspecialidadeSerializer
-    permission_classes = [permissions.IsAuthenticated, IsAdmin]
+    # permission_classes = [permissions.IsAuthenticated, IsAdmin]
 
 
 class VagaViewSet(viewsets.ModelViewSet):
 
     queryset = models.Vaga.objects.all()
     serializer_class = serializers.VagaSerializer
-    permission_classes = [AppPermision]
+    # permission_classes = [AppPermision]
 
 
 class CandidatoVagaViewSet(viewsets.ModelViewSet):
 
     queryset = models.CandidatoVaga.objects.all()
     serializer_class = serializers.CanddatoVagaSerializer
-    permission_classes = [AppPermision]
+    # permission_classes = [AppPermision]
 
 
 class AvaliacaoEventoViewSet(viewsets.ModelViewSet):
 
     queryset = models.AvaliacaoEvento.objects.all()
     serializer_class = serializers.AvaliacaoEventoSerializer
-    permission_classes = [AppPermision]
+    # permission_classes = [AppPermision]
