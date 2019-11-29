7fd0a04dad8a4e642cc441fd4c6c1f6a4365a822
marlonfernando16 <marlon.fernando16@outlook.com>
2019-11-29 16:37:10 -0300

    Define variável de ambiente para a SECRET_KEY

--- simple_events_backend/settings.py
+++ simple_events_backend/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
-
+from decouple import config
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -20,7 +20,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'vakac!t&63j8vco@x2e#a=%!4ucbm%+#_k1vi(2ao6j9u&4c^@'
+SECRET_KEY = config('SECRET_KEY')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
