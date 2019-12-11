1d02dbbbffd8c8a2e119c1aad03c8d76684474ec
marlonfernando16 <marlon.fernando16@outlook.com>
2019-12-10 22:12:56 -0300

    Requisita o usuário se autenticar ou só obter os recursos

--- api/views.py
+++ api/views.py
@@ -1,6 +1,5 @@
 
-from rest_framework import viewsets, permissions, status
-from rest_framework.response import Response
+from rest_framework import viewsets, permissions
 
 from . import models
 from . import serializers
@@ -17,7 +16,7 @@ class UserViewSet(viewsets.ModelViewSet):
 class EventoViewSet(viewsets.ModelViewSet):
 
     queryset = models.Evento.objects.all()
-    permission_classes = [AppPermision]
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
 
     serializer_class = serializers.EventoSerializer
     # permission_classes = [AppPermision]
@@ -28,24 +27,25 @@ class EspecialidadeViewSet(viewsets.ModelViewSet):
     queryset = models.Especialidade.objects.all()
     serializer_class = serializers.EspecialidadeSerializer
     # permission_classes = [permissions.IsAuthenticated, IsAdmin]
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
 
 
 class VagaViewSet(viewsets.ModelViewSet):
 
     queryset = models.Vaga.objects.all()
     serializer_class = serializers.VagaSerializer
-    # permission_classes = [AppPermision]
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
 
 
 class CandidatoVagaViewSet(viewsets.ModelViewSet):
 
     queryset = models.CandidatoVaga.objects.all()
     serializer_class = serializers.CanddatoVagaSerializer
-    # permission_classes = [AppPermision]
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
 
 
 class AvaliacaoEventoViewSet(viewsets.ModelViewSet):
 
     queryset = models.AvaliacaoEvento.objects.all()
     serializer_class = serializers.AvaliacaoEventoSerializer
-    # permission_classes = [AppPermision]
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
