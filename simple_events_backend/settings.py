9ac2e83da76583be3557284074024790ba9cbfa2
marlonfernando16 <marlon.fernando16@outlook.com>
2019-12-10 22:08:07 -0300

    Configura django rest para usar autenticação por token

--- simple_events_backend/settings.py
+++ simple_events_backend/settings.py
@@ -43,8 +43,17 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'api',
     'rest_framework',
+    'rest_framework.authtoken',
     'corsheaders',
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    )
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
